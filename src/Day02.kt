b6d999370cc90c1a66158803dd69af540944da4b
Neil Sainsbury <neil.sainsbury@foxtel.com.au>
2022-12-05 14:12:56 +1000

    Completed Day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -3,51 +3,78 @@ import java.io.File
 enum class Shape(val score: Int) {
     ROCK(1),
     PAPER(2),
-    SCISSORS(3)
+    SCISSORS(3);
+
+    val beats: Shape
+        get() = when (this) {
+            ROCK -> SCISSORS
+            PAPER -> ROCK
+            SCISSORS -> PAPER
+        }
 }
 
-data class Round(val opponentShape: Shape, val myShape: Shape) {
-    fun calculateScore(): Int {
-        val winLossScore = if (
-            (myShape == Shape.ROCK && opponentShape == Shape.SCISSORS) ||
-            (myShape == Shape.SCISSORS && opponentShape == Shape.PAPER) ||
-            (myShape == Shape.PAPER && opponentShape == Shape.ROCK)
-        ) {
-            6 // Win
-        } else if (myShape == opponentShape) {
-            3 // Draw
+enum class Outcome(val score: Int) {
+    WIN(6),
+    DRAW(3),
+    LOSS(0)
+}
+
+class Round(input: String, firstStrategy: Boolean = true) {
+
+    private val opponentMapping = mutableMapOf(
+        "A" to Shape.ROCK,
+        "B" to Shape.PAPER,
+        "C" to Shape.SCISSORS
+    )
+
+    private val myMapping = mutableMapOf(
+        "X" to Shape.ROCK,
+        "Y" to Shape.PAPER,
+        "Z" to Shape.SCISSORS
+    )
+
+    private val outcomeMapping = mutableMapOf(
+        "X" to Outcome.LOSS,
+        "Y" to Outcome.DRAW,
+        "Z" to Outcome.WIN
+    )
+
+    private val opponentShape: Shape
+    private val myShape: Shape
+    private val outcome: Outcome
+
+    val score
+        get() = outcome.score + myShape.score
+
+    init {
+        val (firstColumn, secondColumn) = input.split(" ")
+        opponentShape = opponentMapping[firstColumn]!!
+
+        if (firstStrategy) {
+            myShape = myMapping[secondColumn]!!
+
+            outcome = when {
+                myShape.beats == opponentShape -> Outcome.WIN
+                myShape == opponentShape -> Outcome.DRAW
+                else -> Outcome.LOSS
+            }
         } else {
-            0
-        }
+            outcome = outcomeMapping[secondColumn]!!
 
-        return winLossScore + myShape.score
-    }
-
-    companion object {
-        private val opponentMapping = mutableMapOf(
-            "A" to Shape.ROCK,
-            "B" to Shape.PAPER,
-            "C" to Shape.SCISSORS
-        )
-
-        private val myMapping = mutableMapOf(
-            "X" to Shape.ROCK,
-            "Y" to Shape.PAPER,
-            "Z" to Shape.SCISSORS
-        )
-
-        fun fromInput(str: String): Round {
-            val (opponentChoice, myChoice) = str.split(" ")
-
-            return Round(opponentMapping[opponentChoice]!!, myMapping[myChoice]!!)
+            myShape = when (outcome) {
+                Outcome.WIN -> Shape.values().first { it.beats == opponentShape }
+                Outcome.DRAW -> opponentShape
+                Outcome.LOSS -> opponentShape.beats
+            }
         }
     }
 }
 
 fun main() {
-    val scores = File("data/day02_input.txt")
+    File("data/day02_input.txt")
         .readLines()
-        .map { Round.fromInput(it).calculateScore() }
-
-    println(scores.sum())
+        .also { rounds ->
+            println(rounds.sumOf { Round(it, firstStrategy = true).score }) // Part 1
+            println(rounds.sumOf { Round(it, firstStrategy = false).score }) // Part 2
+        }
 }
