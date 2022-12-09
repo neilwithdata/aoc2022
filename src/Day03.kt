5beed0e089bd7af99ac36e39c1cd3e0f0bfe0285
Neil Sainsbury <neil.sainsbury@foxtel.com.au>
2022-12-09 11:43:00 +1000

    Completed Day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,18 +1,37 @@
 import java.io.File
 
+fun Char.priority(): Int =
+    if (this.isLowerCase()) {
+        1 + this.code - 'a'.code
+    } else {
+        27 + this.code - 'A'.code
+    }
+
+
 fun main() {
-    println(File("data/day03_input.txt")
+    val input = File("data/day03_input.txt")
         .readLines()
-        .sumOf { line ->
-            val firstHalf = line.substring(0, line.length / 2)
-            val secondHalf = line.substring(line.length / 2)
 
-            val common = firstHalf.toSet().intersect(secondHalf.toSet()).first()
+    // Part 1
+    println(input.sumOf { line ->
+        val firstHalf = line.substring(0, line.length / 2)
+        val secondHalf = line.substring(line.length / 2)
 
-            if (common.isLowerCase()) {
-                1 + common.code - 'a'.code
-            } else {
-                27 + common.code - 'A'.code
-            }
-        })
+        findCommonChars(firstHalf, secondHalf).first().priority()
+    })
+
+    // Part 2
+    println(input.chunked(3) { group ->
+        findCommonChars(*group.toTypedArray()).first().priority()
+    }.sum())
+}
+
+fun findCommonChars(vararg strings: String): Set<Char> {
+    var common = strings.first().toSet()
+
+    for (string in strings.drop(1)) {
+        common = common intersect string.toSet()
+    }
+
+    return common
 }
