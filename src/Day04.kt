ebc2bea24b8eadf63db43f71961efc843617e134
Neil Sainsbury <neil.sainsbury@foxtel.com.au>
2022-12-15 10:12:53 +1000

    Completed Day 4

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,17 +1,30 @@
 import java.io.File
 
 fun main() {
+    val fullyContained = pairCount { min1, max1, min2, max2 ->
+        ((min1 <= min2) && (max1 >= max2)) || ((min2 <= min1) && (max2 >= max1))
+    }
+
+    // Part 1
+    println(fullyContained)
+
+    val overlapping = pairCount { min1, max1, min2, max2 ->
+        (min1..max1 intersect min2..max2).isNotEmpty()
+    }
+
+    // Part 2
+    println(overlapping)
+}
+
+fun pairCount(predicate: (min1: Int, max1: Int, min2: Int, max2: Int) -> Boolean): Int {
     val regex = Regex("""(\d+)-(\d+),(\d+)-(\d+)""")
 
-    val res = File("data/day04_input.txt")
+    return File("data/day04_input.txt")
         .readLines()
         .count { line ->
             val match = requireNotNull(regex.find(line))
             val (min1, max1, min2, max2) = match.groupValues.drop(1).map { it.toInt() }
 
-            return@count ((min1 <= min2) && (max1 >= max2)) ||
-                    ((min2 <= min1) && (max2 >= max1))
+            predicate(min1, max1, min2, max2)
         }
-
-    println(res)
 }
